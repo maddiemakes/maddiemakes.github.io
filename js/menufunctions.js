4f25f6b4f67838d01a4d7f6638978d4762518f1a
Jordan <jwells.me@gmail.com>
2017-01-07 01:59:15 -0500

    Add files via upload

--- js/menufunctions.js
+++ js/menufunctions.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
     
+    function openMenu() {
+        $('.nav-trigger').attr('checked', true);
+    }
+    
     function closeMenu() {
         $('.nav-trigger').attr('checked', false);
     }
@@ -9,4 +13,17 @@ $(document).ready(function () {
     });
     
     window.onscroll = closeMenu;
+    
+    $(document).on("swipeleft swiperight", ".site-wrap", function(e) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ($.mobile.activePage.jqmData("panel") !== "open") {
+            if (e.type === "swipeleft" ) {
+                closeMenu();
+            } else if (e.type === "swiperight") {
+                openMenu();
+            }
+        }
+    });
 });
